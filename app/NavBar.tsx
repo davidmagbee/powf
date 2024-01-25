bf91eb73b63ca249bea80bd711f58ef9f1bccf9c
“davidmagbee” <david.magbee@me.com>
2024-01-24 21:32:56 -0500

    active link styling - navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BiSolidBug } from "react-icons/bi";
+import classnames from 'classnames'
 
 const NavBar = () => {
+    const currentPath = usePathname()
+
     const links = [
         { label: 'Dashboard', href: '/' },
         { label: 'Issues', href: '/issues' },
@@ -14,10 +20,16 @@ const NavBar = () => {
 
         </Link>
         <ul className='flex space-x-6'>
-            {links.map(link => <Link 
-            key={link.href} 
-            className='text-zinc-300 hover:text-zinc-600 transition-colors' 
-            href={link.href}>{link.label}</Link>)}
+            {links.map(link => 
+            <Link 
+                key={link.href} 
+                className={classnames({
+                    'text-zinc-500': link.href === currentPath,
+                    'text-zinc-300': link.href !== currentPath,
+                    'hover:text-zinc-600 transition-colors': true
+                })} 
+                href={link.href}>{link.label}
+            </Link>)}
         </ul>
     </nav>
   )
