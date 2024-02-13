fae9afa5e5953b0e6e88b98d6a0d4a57884bb6ac
“davidmagbee” <david.magbee@me.com>
2024-02-13 12:54:04 -0500

    add login/log out links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -5,9 +5,12 @@ import { usePathname } from 'next/navigation';
 import React from 'react'
 import { BiSolidBug } from "react-icons/bi";
 import classnames from 'classnames'
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 const NavBar = () => {
     const currentPath = usePathname()
+    const { status, data: session } = useSession()
 
     const links = [
         { label: 'Dashboard', href: '/' },
@@ -21,16 +24,24 @@ const NavBar = () => {
         </Link>
         <ul className='flex space-x-6'>
             {links.map(link => 
-            <Link 
-                key={link.href} 
+           <li key={link.href} >
+           <Link 
                 className={classnames({
                     'text-zinc-600': link.href === currentPath,
                     'text-zinc-500': link.href !== currentPath,
                     'hover:text-zinc-900 transition-colors': true
                 })} 
                 href={link.href}>{link.label}
-            </Link>)}
+            </Link></li>)}
         </ul>
+        <Box>
+            { status === 'authenticated' && (
+            <Link href='/api/auth/signout'>Log Out</Link>
+            )}
+            { status === 'unauthenticated'  && (
+                <Link href='/api/auth/signin'>Login</Link>
+            ) }
+        </Box>
     </nav>
   )
 }
