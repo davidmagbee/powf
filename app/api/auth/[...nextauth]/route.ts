0e1c5d814de6c65cb89b04a614c966161527e118
“davidmagbee” <david.magbee@me.com>
2024-02-14 10:57:11 -0500

    secure app

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,6 @@
+import authOptions from "@/app/auth/authOptions";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client";
-import prisma from "@/prisma/client";
 
-console.log("Google Client ID:", process.env.GOOGLE_CLIENT_ID);
-console.log("Google Client Secret:", process.env.GOOGLE_CLIENT_SECRET);
-
-
-const handler = NextAuth({
-    
-
-    adapter: PrismaAdapter(prisma),
-    providers: [
-
-        
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID!,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-          })
-    ],
-    session: {
-        strategy: 'jwt'
-    }
-})
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
